28d16647fdec510a8a139966c569be4e688ca66a
sheng67 <1439986513@qq.com>
2026-03-21 18:06:39 +0800

    web配置类

---
+++ server/src/main/java/com/itsheng/service/config/WebMvcConfiguration.java
@@ -0,0 +1,47 @@
+package com.itsheng.service.config;
+
+import com.itsheng.service.interceptor.JwtTokenInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+  private final JwtTokenInterceptor jwtTokenInterceptor;
+
+
+  //注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.info("开始注册自定义拦截器...");
+        registry.addInterceptor(jwtTokenInterceptor)
+                // TODO 要拦截所有请求
+//                .addPathPatterns("/user/**")
+                .excludePathPatterns("/api/user/login",
+                        "/api/user/register",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui.html",
+                        "/doc.html",
+                        "/webjars/**",
+                        "/error");
+    }
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("web端接口")
+                .packagesToScan("com.itsheng.service.controller")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}
