38753c28c44e0944c93e221ee4a04d095e544ee5
sheng67 <1439986513@qq.com>
2026-03-23 16:11:15 +0800

    拦截器拦截所有

--- server/src/main/java/com/itsheng/service/config/WebMvcConfiguration.java
+++ server/src/main/java/com/itsheng/service/config/WebMvcConfiguration.java
@@ -23,8 +23,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenInterceptor)
-                // TODO 要拦截所有请求
-//                .addPathPatterns("/user/**")
+                .addPathPatterns("/api/**")
                 .excludePathPatterns("/api/user/login",
                         "/api/user/register",
                         "/swagger-ui/**",
