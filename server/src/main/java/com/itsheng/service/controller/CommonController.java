f6cab19fe3ada90dc8d07cb81be7f1bb28076ec3
sheng67 <1439986513@qq.com>
2026-03-23 22:51:51 +0800

    配置阿里云oss以及文件上传功能,相关密钥没有配置需要手动配置

---
+++ server/src/main/java/com/itsheng/service/controller/CommonController.java
@@ -0,0 +1,49 @@
+package com.itsheng.service.controller;
+
+
+import com.itsheng.common.result.Result;
+import com.itsheng.common.utils.AliOssUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+//通用接口
+@RestController
+@RequestMapping("/api/common")
+@Tag(name="通用接口")
+@Slf4j
+@RequiredArgsConstructor
+public class CommonController {
+
+
+    private  final AliOssUtil aliOssUtil;
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "文件上传", description = "上传文件并返回访问 URL")
+    public Result<String> upload(@RequestPart("file") MultipartFile file)
+    {
+        log.info("文件上传 :{}",file);
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extention = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String newFileName = UUID.randomUUID().toString() + extention;
+            String filePath = aliOssUtil.upload(file.getBytes(), newFileName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败{}",e);
+        }
+        return null;
+    }
+
+
+}
